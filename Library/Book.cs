20cb91a7f22427da8f95b8c3ad9f5af85bcff662
banan <edem0004@edu.easj.dk>
2021-10-14 22:53:53 +0200

    Assignment 1

---
+++ Library/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Library
+{
+    public class Book
+    {
+
+        public string ISBN13 { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
